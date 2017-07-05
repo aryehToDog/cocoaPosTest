348dbe84e09d9c0ecc215e5e04cf2c62bc03965a
阿拉斯加的狗 <1281433811@qq.com>
2017-07-05 11:38:29 +0800

    first commit

---
+++ CocoaposMainTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaposMainTest
+//
+//  Created by 阿拉斯加的狗 on 2017/7/5.
+//  Copyright © 2017年 阿拉斯加的🐶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaposMainTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaposMainTest
+//
+//  Created by 阿拉斯加的狗 on 2017/7/5.
+//  Copyright © 2017年 阿拉斯加的🐶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
